3b91d952d61dab54cf71063ab93243171b13d91c
Dave Strider <evan.ebert17@gmail.com>
2021-10-26 18:58:57 -0500

    updated boiler code for project

--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import NavTabs from './NavTabs';
-import Home from './pages/Home';
+import NavTabs from './navtabs';
+import Home from './pages/About';
 import Resume from './pages/Resume';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
@@ -12,8 +12,8 @@ export default function PortfolioContainer() {
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
+    if (currentPage === 'About') {
+      return <About />;
     }
     if (currentPage === 'Projects') {
       return <Projects />;
